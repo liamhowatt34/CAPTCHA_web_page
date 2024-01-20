782c59c3624433e550e5aa5312dbbb7411747ee1
Liam Howatt <liamhowatt34@gmail.com>
2024-01-19 21:49:05 -0500

    Added check button functionality and correct alert pop-up.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const screen = document.querySelector('.screen');
 const regenBtn = document.querySelector('.regen');
+const checkBtn = document.querySelector('.btn');
+const inputField = document.querySelector('.form-control');
+const message = document.querySelector('.alert');
 
 
 function generateCode() {
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let code = "";
 
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 6; i++) {
         let randomInt = Math.floor(Math.random() * 62);
         code += chars[randomInt];
     }
@@ -15,10 +18,23 @@ function generateCode() {
 }
 
 let initialCode = generateCode();
-
 screen.innerText = initialCode;
+
 
 regenBtn.addEventListener('click', () => {
     let newCode = generateCode();
     screen.innerText = newCode;
+});
+
+checkBtn.addEventListener('click', () => {
+    let inputValue = inputField.value.trim();
+    message.classList.remove('alert-success', 'alert-danger');
+
+    if (screen.innerText === inputValue) {
+        message.classList.toggle('alert-success');
+        message.innerHTML = 'Success!';
+    } else {
+        message.classList.toggle('alert-danger');
+        message.innerHTML = 'Incorrect..';
+    }
 });
